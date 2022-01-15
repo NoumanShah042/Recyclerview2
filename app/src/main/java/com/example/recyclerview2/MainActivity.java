6667364a71f77bb5a16c01acc8de8add51bdc879
NoumanShah042 <noumanrehman042@gmail.com>
2022-01-15 15:16:00 +0500

    codes here

--- app/src/main/java/com/example/recyclerview2/MainActivity.java
+++ app/src/main/java/com/example/recyclerview2/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.recyclerview2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView rcv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rcv =  findViewById(R.id.rclview);
+        rcv.setLayoutManager(new LinearLayoutManager(this));
+
+        String arr[] = {"C", "C++", "JAVA", "PHP", ".NEt", "JQuery", "C", "C++", "JAVA", "PHP", ".NEt", "JQuery", "C", "C++", "JAVA", "PHP", ".NEt", "JQuery"};
+        rcv.setAdapter(new myAdapter(arr));
+
     }
 }
---
+++ app/src/main/java/com/example/recyclerview2/myAdapter.java
@@ -0,0 +1,65 @@
+package com.example.recyclerview2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class myAdapter extends RecyclerView.Adapter<myAdapter.MyViewHolder> {
+    String data[];
+
+    public myAdapter(String[] data) {    // constructor
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.singlerow, parent, false);
+
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.tv.setText(data[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }
+
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView img;
+        TextView tv;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            img = (ImageView) itemView.findViewById(R.id.img1);
+            tv = (TextView) itemView.findViewById(R.id.t1);
+        }
+    }
+
+}
+
+/*
+*    1) create class and extend  RecyclerView.Adapter:
+*           class myAdapter extends RecyclerView.Adapter
+*   2) create your view holder ( MyViewHolder ) and initializes all the views with your singleRow layout views id
+*           MyViewHolder extends RecyclerView.ViewHolder
+*
+*    3) add MyViewHolder to RecyclerView.Adapter<myAdapter.MyViewHolder>
+*       class myAdapter extends RecyclerView.Adapter<myAdapter.MyViewHolder>
+
+*   4) now implement the other methods
+*
+*
+* */
